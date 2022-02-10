1a2e27d6f653b18cb31abcfaa99b9fd3b884ce18
Kush Mansingh <12158241+kushmansingh@users.noreply.github.com>
2022-02-10 10:48:06 -0500

    Add GH action for deployment  (#1)
    * Add cloudfront terraform
    
    * Add GH action
    
    * Add docs
    
    Co-authored-by: Kushagra Mansingh <8812648-kushmansingh@users.noreply.gitlab.com>

---
+++ terraform/iam.tf
@@ -0,0 +1,36 @@
+resource "aws_iam_user" "ci" {
+  name = "github-docs-ci"
+  tags = local.tags
+}
+
+resource "aws_iam_user_policy" "lb_ro" {
+  user = aws_iam_user.ci.name
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "s3:*"
+      ],
+      "Effect": "Allow",
+      "Resource": "${aws_s3_bucket.bucket.arn}/*"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_access_key" "ci" {
+  user = aws_iam_user.ci.name
+}
+
+output "ci_access_key" {
+    value = aws_iam_access_key.ci.id
+}
+
+output "ci_secret_key" {
+    sensitive = true
+    value = aws_iam_access_key.ci.secret
+}
