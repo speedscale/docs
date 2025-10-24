fa37eb7ec9386c2b2779d7956d6285ae45aaa35b
Ken Ahrens <ken.ahrens@gmail.com>
2025-10-24 15:56:13 -0400

    fix 404 (#575)

--- src/components/Cards/CICDIntegrationCard.js
+++ src/components/Cards/CICDIntegrationCard.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Link from '@docusaurus/Link';
 import Card from '@site/src/components/Card';
 import CardHeader from '@site/src/components/Card/CardHeader';
 import CardBody from '@site/src/components/Card/CardBody';
@@ -15,7 +16,7 @@ const CICDIntegrationCard = () => {
         <h3 style={{ margin: 0, marginBottom: '1rem' }}>🔄 CI/CD Integration</h3>
       </CardHeader>
       <CardBody className="padding-vert--md">
-        Integrate proxymock into your <a href="./integration/cicd/cicd">CI/CD pipeline</a> to automate testing and ensure API reliability in your deployment process.
+        Integrate proxymock into your <Link to="/proxymock/guides/cicd">CI/CD pipeline</Link> to automate testing and ensure API reliability in your deployment process.
       </CardBody>
       <CardFooter style={{ backgroundColor: '#f8f9fa', textAlign: 'center' }}>
         <a href="https://proxymock.io/blog/automating-api-mocks-in-your-ci-pipeline-with-proxymock/" className="button button--secondary">Learn CI/CD</a>
